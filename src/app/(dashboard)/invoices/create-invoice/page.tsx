5ca2e77cdc1f3a6165e17c2298170494fc957f14
marlonjs <marlondbem@icloud.com>
2024-06-23 21:55:44 -0300

    invoice preview added

--- src/app/(dashboard)/invoices/create-invoice/page.tsx
+++ src/app/(dashboard)/invoices/create-invoice/page.tsx
@@ -5,7 +5,63 @@ export default function CreateInvoice() {
     <div className="h-full">
       <div className="flex h-full">
         <div className="w-1/2 flex flex-col">
-          <div className="bg-gray-50 h-full px-6 flex items-center justify-center"></div>
+          <div className="bg-gray-50 h-full px-6 pt-6 flex items-center justify-center">
+            <div className="border p-14 rounded-t-xl bg-white drop-shadow-xl w-full h-full">
+              <h2 className="text-2xl font-bold mb-6 text-spaceblack">Invoice</h2>
+              <div className="mb-6">
+                <p className="text-sm text-gray-500">Client name</p>
+                <p className="text-md text-spaceblack">Amazon, Inc.</p>
+              </div>
+              <div className="mb-6">
+                <p className="text-sm text-gray-500">Invoice number</p>
+                <p className="text-md text-spaceblack">#0001</p>
+              </div>
+              <div className="flex justify-between mb-6">
+                <div>
+                  <p className="text-sm text-gray-500">Invoice date</p>
+                  <p className="text-md text-spaceblack">06/23/2024</p>
+                </div>
+                <div>
+                  <p className="text-sm text-gray-500">Due date</p>
+                  <p className="text-md text-spaceblack">07/22/2024</p>
+                </div>
+              </div>
+              <div className="mb-6">
+                <p className="text-sm text-gray-500">Amount</p>
+                <p className="text-md text-spaceblack">$200.00</p>
+              </div>
+              <div className="mb-6">
+                <p className="text-sm text-gray-500">Items</p>
+                <table className="w-full text-left text-sm border-collapse">
+                  <thead>
+                    <tr>
+                      <th className="border-b pb-2">Item</th>
+                      <th className="border-b pb-2">Description</th>
+                      <th className="border-b pb-2">Quantity</th>
+                      <th className="border-b pb-2">Price</th>
+                      <th className="border-b pb-2">Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="pt-2 pb-2 border-b">Plumber</td>
+                      <td className="pt-2 pb-2 border-b">A good service done</td>
+                      <td className="pt-2 pb-2 border-b">2</td>
+                      <td className="pt-2 pb-2 border-b">$50.00</td>
+                      <td className="pt-2 pb-2 border-b">$100.00</td>
+                    </tr>
+                    <tr>
+                      <td className="pt-2 pb-2 border-b">Support</td>
+                      <td className="pt-2 pb-2 border-b">Great job</td>
+                      <td className="pt-2 pb-2 border-b">1</td>
+                      <td className="pt-2 pb-2 border-b">$100.00</td>
+                      <td className="pt-2 pb-2 border-b">$100.00</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="flex-1 border-l flex justify-center px-14 py-6">
           <form className="max-w-xl w-full">
