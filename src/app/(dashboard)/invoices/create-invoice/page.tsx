ba9bed1f538a26ef71a5c0706aa604a34840b412
marlonjs <marlondbem@icloud.com>
2024-06-24 21:52:49 -0300

    feat: invoice preview

--- src/app/(dashboard)/invoices/create-invoice/page.tsx
+++ src/app/(dashboard)/invoices/create-invoice/page.tsx
@@ -1,27 +1,11 @@
-'use client'
-
-import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { InvoicePreview } from "./invoice-preview";
 import { Label } from "@/components/ui/label";
-import { Datepicker } from "@/components/ui/datepicker";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { ChevronRightIcon, PlusIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 
 export default function CreateInvoice() {
-  const [items, setItems] = useState([])
-
-  const addItem = () => {
-    setItems([...items, ''])
-  }
-
-  const handleItemChange = ({index, value}: any) => {
-    const newItems = [...items];
-    newItems[index] = value;
-    setItems(newItems);
-  }
-
   return (
     <div className="h-full">
       <div className="flex h-full">
@@ -35,41 +19,15 @@ export default function CreateInvoice() {
             <div className="flex flex-col gap-4 h-full">
               <div className="flex flex-col gap-2 font-medium text-spaceblack">
                 <Label htmlFor="client">Client name</Label>
-                <Input type="text" id="client"/>
+                <Input type="text" id="client" />
               </div>
               <div className="flex flex-col gap-2 font-medium">
                 <Label htmlFor="invoice-number">Invoice number</Label>
                 <Input type="text" id="invoice-number" />
               </div>
-              <div className="flex w-full gap-4 justify-between">
-                <div className="flex flex-col w-full gap-2 font-medium">
-                  <Label htmlFor="invoice-date">Invoice date</Label>
-                  <Datepicker />
-                </div>
-                <div className="flex flex-col w-full gap-2 font-medium">
-                  <Label htmlFor="due-date">Due date</Label>
-                  <Datepicker />
-                </div>
-              </div>
               <div className="flex flex-col gap-2 font-medium">
                 <Label htmlFor="amount">Amount</Label>
                 <Input type="number" id="amount" />
-              </div>
-              <div className="flex flex-col gap-2 font-medium">
-                <Label>Products and services</Label>
-                {items.map((item, index) => (
-                  <Input
-                    key={index}
-                    type="text"
-                    value={item}
-                    onChange={(e) => handleItemChange(index, e.target.value)}
-                    className="mb-2"
-                  />
-                ))}
-                <Button color="ghost" type="button" onClick={addItem}>
-                  <PlusIcon className="mr-2 h-4 w-4 stroke-2" />
-                  Add an item
-                </Button>
               </div>
             </div>
             <div className="flex justify-end gap-6">
